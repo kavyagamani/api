222fb7760c0607810da0f6fdadae93e1e1b923d8
kavya Hegde <151379092+kavyagamani@users.noreply.github.com>
2026-02-10 18:49:22 +0530

    Delete karate-tests directory

--- karate-tests/src/test/resources/cucumber/features/sample.feature
+++
@@ -1,6 +0,0 @@
-Feature: Sample addition
-
-  Scenario: add two numbers
-    Given a number 1
-    When I add 2
-    Then the result should be 3
--- karate-tests/src/test/resources/karate-config.js
+++
@@ -1,6 +0,0 @@
-function fn() {
-  var config = {};
-  // default base URL; can be overridden with -DbaseUrl=http://localhost:8080
-  config.baseUrl = karate.properties['baseUrl'] || 'http://localhost:8080';
-  return config;
-}
